ef65d039db5260968758234a140e107d6f7ef6b2
tohirjonovhadyatilo <mobiletillopubg@gmail.com>
2025-02-01 22:17:39 +0500

    add minify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: false, // Source map’larni o‘chiradi, kodni yashiradi
+    minify: 'terser', // Kodni minifikatsiya qiladi
+    terserOptions: {
+      compress: {
+        drop_console: true, // Console.log'larni olib tashlaydi
+        drop_debugger: true, // Debugger'larni olib tashlaydi
+      },
+      mangle: true, // O'zgaruvchilar nomini chalkashtirib tashlaydi
+    },
+  },
+});
